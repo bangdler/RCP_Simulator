54df4c50bb1603aef45a28852ac49ca3e7128a99
bangdler <zbthz90@gmail.com>
2023-01-18 22:31:04 +0900

    refactor: canvas 컴포넌트에서 handTable 분리

---
+++ src/Hand/HandTable.ts
@@ -0,0 +1,136 @@
+import { Hand, I_Hand } from '@/Hand/Hand';
+import { DRAW, FIGHTING, LOSE, WIN } from '@/Hand/constants';
+import { getRandomNumExcludeMax } from '@/utils/utils';
+
+export interface I_HandTable {
+  handTable: { [index: number]: I_Hand };
+  numOfHands: { [index: string]: number };
+  animateHands: (ctx: CanvasRenderingContext2D) => void;
+  resetTable: () => void;
+}
+type HandTable_Constructor = {
+  initNum: number;
+  handTypes: Array<string>;
+  width: number;
+  height: number;
+  size: number;
+};
+export class HandTable implements I_HandTable {
+  handTable: { [index: number]: I_Hand };
+  numOfHands: { [index: string]: number };
+  readonly initNum: number;
+  readonly handTypes: Array<string>;
+  readonly width: number;
+  readonly height: number;
+  readonly size: number;
+  constructor({
+    initNum,
+    handTypes,
+    width,
+    height,
+    size,
+  }: HandTable_Constructor) {
+    this.initNum = initNum;
+    this.handTypes = handTypes;
+    this.width = width;
+    this.height = height;
+    this.size = size;
+    this.numOfHands = {
+      [this.handTypes[0]]: this.initNum,
+      [this.handTypes[1]]: this.initNum,
+      [this.handTypes[2]]: this.initNum,
+    };
+    this.handTable = this.makeHandTable();
+  }
+
+  makeHandTable(): { [index: number]: I_Hand } {
+    const obj: { [index: number]: I_Hand } = {};
+    for (let i = 0; i < this.initNum * this.handTypes.length; i++) {
+      obj[i] = new Hand({
+        id: i,
+        startX: getRandomNumExcludeMax(this.size, this.width - this.size),
+        startY: getRandomNumExcludeMax(this.size, this.height - this.size),
+        type: this.handTypes[i % this.handTypes.length],
+        limitX: this.width,
+        limitY: this.height - this.size,
+        size: this.size,
+      });
+    }
+    return obj;
+  }
+
+  getOverlapTable(): { [index: number]: Array<string> } {
+    const overlapTable: { [index: number]: Array<string> } = {};
+    for (let i in this.handTable) {
+      const cur = this.handTable[i];
+      for (let j in this.handTable) {
+        const compare = this.handTable[j];
+        if (cur.id === compare.id) continue;
+        const [diffX, diffY] = [
+          Math.abs(cur.curX - compare.curX),
+          Math.abs(cur.curY - compare.curY),
+        ];
+        if (diffX > this.size || diffY > this.size) continue;
+        // 겹치는 경우 대결을 진행하고 결과값을 배열에 넣는다.
+        const fightResult = cur.fight(compare.type);
+        if (!overlapTable[cur.id]) {
+          overlapTable[cur.id] = [fightResult];
+        } else {
+          overlapTable[cur.id].push(fightResult);
+        }
+      }
+    }
+    return overlapTable;
+  }
+
+  setFightingEffect(ctx: CanvasRenderingContext2D, x: number, y: number) {
+    ctx.font = `${this.size}px serif`;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(FIGHTING, x, y);
+  }
+
+  setFightResultIfOverlap(ctx: CanvasRenderingContext2D) {
+    const overlapTable = this.getOverlapTable();
+    for (let idx in overlapTable) {
+      const overlappedHand = this.handTable[idx];
+      const fightResults = overlapTable[idx];
+
+      if (fightResults.includes(LOSE) && !fightResults.includes(WIN)) {
+        overlappedHand.setFightResult(LOSE);
+      } else if (fightResults.includes(WIN) && !fightResults.includes(LOSE)) {
+        overlappedHand.setFightResult(WIN);
+      } else {
+        overlappedHand.setFightResult(DRAW);
+      }
+      if (!overlappedHand.first) {
+        this.setFightingEffect(ctx, overlappedHand.curX, overlappedHand.curY);
+      } else {
+        overlappedHand.setFirst(false);
+      }
+    }
+  }
+
+  animateHands(ctx: CanvasRenderingContext2D) {
+    for (let idx in this.handTable) {
+      const hand = this.handTable[idx];
+      if (hand.fightResult === LOSE) {
+        this.numOfHands[hand.type] -= 1;
+        delete this.handTable[idx];
+        continue;
+      }
+      hand.animate(ctx);
+      hand.randomMove();
+    }
+    this.setFightResultIfOverlap(ctx);
+  }
+
+  resetTable() {
+    this.numOfHands = {
+      [this.handTypes[0]]: this.initNum,
+      [this.handTypes[1]]: this.initNum,
+      [this.handTypes[2]]: this.initNum,
+    };
+    this.handTable = this.makeHandTable();
+  }
+}
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
 import { useCanvas } from '@/hooks/useCanvas';
-import { IRCP, RCP } from '@/RCP/RCP';
+import { I_Hand, Hand } from '@/Hand/Hand';
 import {
-  RCP_SIZE,
-  RCP_TYPES,
-  FIGHTING,
+  HAND_SIZE,
+  HAND_TYPES,
   LOSE,
   WIN,
   DRAW,
   INIT_NUM,
-} from '@/RCP/constants';
+} from '@/Hand/constants';
 import { getRandomNumExcludeMax } from '@/utils/utils';
-import styled from 'styled-components';
+import { HandTable, I_HandTable } from '@/Hand/HandTable';
 
 type CanvasProps = {
   canvasWidth: number;
@@ -20,125 +20,92 @@ type CanvasProps = {
 };
 
 export default function Canvas({ canvasWidth, canvasHeight }: CanvasProps) {
-  const numOfRCP = {
-    [RCP_TYPES[0]]: INIT_NUM,
-    [RCP_TYPES[1]]: INIT_NUM,
-    [RCP_TYPES[2]]: INIT_NUM,
-  };
-
-  const makeRcpTable = (): { [index: number]: IRCP } => {
-    const obj: { [index: number]: IRCP } = {};
-    for (let i = 0; i < INIT_NUM * 3; i++) {
-      obj[i] = new RCP({
-        id: i,
-        startX: getRandomNumExcludeMax(RCP_SIZE, canvasWidth - RCP_SIZE),
-        startY: getRandomNumExcludeMax(RCP_SIZE, canvasHeight - RCP_SIZE),
-        type: RCP_TYPES[i % 3],
-        limitX: canvasWidth,
-        limitY: canvasHeight - RCP_SIZE,
-        size: RCP_SIZE,
-      });
-    }
-    return obj;
-  };
-
-  const rcpTable: { [index: number]: IRCP } = makeRcpTable();
-
-  const getOverlaps = (): { [index: number]: Array<string> } => {
-    const overlaps: { [index: number]: Array<string> } = {};
-    for (let i in rcpTable) {
-      const cur = rcpTable[i];
-      for (let j in rcpTable) {
-        const compare = rcpTable[j];
-        if (cur.id === compare.id) continue;
-        const [diffX, diffY] = [
-          Math.abs(cur.curX - compare.curX),
-          Math.abs(cur.curY - compare.curY),
-        ];
-        if (diffX > RCP_SIZE || diffY > RCP_SIZE) continue;
-        // 겹치는 경우 대결을 진행하고 결과값을 배열에 넣는다.
-        const fightResult = cur.fight(compare.type);
-        if (!overlaps[cur.id]) {
-          overlaps[cur.id] = [fightResult];
-        } else {
-          overlaps[cur.id].push(fightResult);
-        }
-      }
-    }
-    return overlaps;
-  };
-
-  const setFightResultIfOverlap = () => {
-    const overlaps = getOverlaps();
-    for (let idx in overlaps) {
-      const overlappedRcp = rcpTable[idx];
-      const fightResults = overlaps[idx];
-
-      if (fightResults.includes(LOSE) && !fightResults.includes(WIN)) {
-        overlappedRcp.setFightResult(LOSE);
-      } else if (fightResults.includes(WIN) && !fightResults.includes(LOSE)) {
-        overlappedRcp.setFightResult(WIN);
-      } else {
-        overlappedRcp.setFightResult(DRAW);
-      }
-    }
-  };
-
-  const animateRCP = (ctx: CanvasRenderingContext2D) => {
-    if (Object.values(numOfRCP).filter(x => x === 0).length === 2) {
-      stopAnimation();
-    }
-    for (let idx in rcpTable) {
-      const rcp = rcpTable[idx];
-      if (rcp.fightResult === LOSE) {
-        numOfRCP[rcp.type] -= 1;
-        delete rcpTable[idx];
-        continue;
-      }
-      rcp.animate(ctx);
-      rcp.randomMove();
-    }
-    setFightResultIfOverlap();
-  };
+  const handTable: I_HandTable = new HandTable({
+    initNum: INIT_NUM,
+    handTypes: HAND_TYPES,
+    width: canvasWidth,
+    height: canvasHeight,
+    size: HAND_SIZE,
+  });
 
   const fillBackground = (ctx: CanvasRenderingContext2D) => {
-    ctx.fillStyle = 'grey';
+    ctx.fillStyle = 'aquamarine';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
   };
 
   const animate = (ctx: CanvasRenderingContext2D) => {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     fillBackground(ctx);
-    animateRCP(ctx);
+    if (Object.values(handTable.numOfHands).filter(x => x === 0).length === 2) {
+      stopAnimation();
+    }
+    handTable.animateHands(ctx);
   };
 
-  const { canvasRef, startAnimation, stopAnimation } = useCanvas(
+  const reset = () => {
+    stopAnimation();
+    handTable.resetTable();
+    setAnimation();
+  };
+
+  const { canvasRef, startAnimation, stopAnimation, setAnimation } = useCanvas(
     canvasWidth,
     canvasHeight,
     animate
   );
 
   useEffect(() => {
-    for (let idx in rcpTable) {
-      const rcp = rcpTable[idx];
-      rcp.updateLimit(canvasWidth - RCP_SIZE, canvasHeight - RCP_SIZE);
+    for (let idx in handTable.handTable) {
+      const hand = handTable.handTable[idx];
+      hand.updateLimit(canvasWidth - HAND_SIZE, canvasHeight - HAND_SIZE);
     }
   }, [canvasWidth, canvasHeight]);
 
   return (
-    <>
-      <canvas ref={canvasRef} />
-      <S_BtnWrapper>
-        <S_Btn onClick={startAnimation}>start</S_Btn>
-        <S_Btn onClick={stopAnimation}>stop</S_Btn>
-      </S_BtnWrapper>
-    </>
+    <S_Wrapper>
+      <S_Title>가위바위보의 승자는 누구?</S_Title>
+      <S_Container>
+        <canvas ref={canvasRef} />
+        <S_BtnContainer>
+          <S_Btn onClick={startAnimation}>start</S_Btn>
+          <S_Btn onClick={stopAnimation}>stop</S_Btn>
+          <S_Btn onClick={reset}>reset</S_Btn>
+        </S_BtnContainer>
+      </S_Container>
+    </S_Wrapper>
   );
 }
 
-const S_BtnWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 500px;
+const S_Wrapper = styled.div`
+  display: flex;
+  flex-flow: column;
 `;
-const S_Btn = styled.button``;
+
+const S_Title = styled.h1`
+  font-size: 20px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const S_Container = styled.div`
+  display: flex;
+  * {
+    margin-right: 10px;
+  }
+`;
+
+const S_BtnContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  *{
+    margin: 10px;
+  }
+`;
+const S_Btn = styled.button`
+  background: aquamarine;
+  font-size: 20px;
+  border-radius: 4px;
+  padding: 5px;
+`;
